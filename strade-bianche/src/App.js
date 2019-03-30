7c0c2da7198c5985d263f2d0ca9775574a622a05
Garrett Swensen <romdame@gmail.com>
2019-03-30 04:06:14 -0600

    after create react app

---
+++ strade-bianche/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="Full-main-header">
+          <p>Hello World!</p>
+        </header>
+        <body>
+          <div>
+            <section className="Full-main-left">
+
+            </section>
+          </div>
+          <div>
+            <section>
+
+            </section>
+          </div>
+        </body>
+      </div>
+    );
+  }
+}
+
+export default App;
